029cc9fae8096695cc400095000d71d70dd1640a
51lv4 <57497075+51lv4@users.noreply.github.com>
2024-02-03 03:30:50 +0000

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'kk-hdd-prices';
+  product: string = '';
+  constructor(private productService: ProductsService) { }
+  fetchProduct(): void {
+    this.productService.getProduct().subscribe((data: any) => {
+      this.product = data.product;
+    });
+  }
 }
